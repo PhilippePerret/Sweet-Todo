3fe727192253ba79942bbcec0be49d291659af87
Philippe Perret <philippe.perret@yahoo.fr>
2020-10-03 06:40:07 +0200

    Changement du nom de l'historique

--- lib/required/TodoFile.rb
+++ lib/required/TodoFile.rb
@@ -107,7 +107,7 @@ class TodoFile
   def add_historique(line)
     line = line[6..-1].strip if line.start_with?('- [x] ')
     @hier_str ||= (Time.now - 3600*24).strftime('%d %m %Y')
-    @refhisto ||= File.open(File.join(APPFOLDER,'historique.txt'),'a')
+    @refhisto ||= File.open(File.join(APPFOLDER,'_Historique_travail_.txt'),'a')
     @refhisto.puts("#{@hier_str} --- #{line}")
   end #/ add_historique
 
